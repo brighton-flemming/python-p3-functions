42fcfd66d72606f18e02310115f789be1447b235
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-17 11:06:30 +0300

    Mission Passed

--- lib/functions.py
+++ lib/functions.py
@@ -3,8 +3,9 @@
 def greet_programmer():
     print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name="Buddy"):
+    print(f"Hello, {name}!")
+greet("Mario")
 
 def greet_with_default(name="programmer"):
     pass
